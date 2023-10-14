a16e72b190faa87726b560d5dc5b2b97b0a3c946
KhushMG <krg04@uw.edu>
2023-10-14 14:59:46 -0700

    landing page

--- App.js
+++ App.js
@@ -2,24 +2,37 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, SafeAreaView, View } from "react-native";
 
 export default function App() {
-  return (
-    // <SafeAreaView className="flex-1 bg-slate-600 items-center">
-    //   <SafeAreaView>
-    //     <Text className="text-white font-bold text-3xl"> Protopet</Text>
-    //   </SafeAreaView>
-    //   <SafeAreaView>
-    //     <Text className="text-white justify-center">App</Text>
-    //   </SafeAreaView>
-    //   <StatusBar style="auto" />
-    // </SafeAreaView>
+  const data = [
+    {
+      name: "Pookie",
+      age: 3,
+      breed: "dog",
+    },
+    {
+      name: "Schmookums",
+      age: 5,
+      breed: "cat",
+    },
+    {
+      name: "C Plus Plus",
+      age: 124,
+      breed: "Creature of Hell"
+    },
+  ];
 
+  return (
     <SafeAreaView className="flex-1 bg-slate-500">
       <SafeAreaView className="items-center">
-        <Text className="text-white font-bold text-2xl"> Protopet </Text>
+        <Text className="text-yellow-500 font-bold text-3xl"> Protopet </Text>
       </SafeAreaView>
       <SafeAreaView className="flex-1 justify-center items-center">
-        <Text className="text-white">App</Text>
+        {data.map((item) => (
+          <Text className="text-white">
+            {item.name} - {item.age} - {item.breed}
+          </Text>
+        ))}
       </SafeAreaView>
+      <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
