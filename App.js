f756114f48fc7689011fa3409755e8f3e329a230
NainooG <khush.nainn@gmail.com>
2023-10-15 08:08:43 -0700

    Vercel

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import LikesScreen from './components2/LikesScreen';
 import HomeScreen from './components2/HomeScreen';
+// import ShelterList from './components2/Shelter'
 
 const Tab = createBottomTabNavigator();
 
@@ -12,6 +13,7 @@ const App = () => {
       <Tab.Navigator>
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Likes" component={LikesScreen} />
+        {/* <Tab.Screen name="Search" component={ShelterList} /> */}
       </Tab.Navigator>
     </NavigationContainer>
   );
---
+++ components2/Shelter.js
@@ -0,0 +1,7 @@
+// import React from 'react';
+// import { View, Text, StyleSheet, Image, FlatList } from 'react-native';
+// import { animals } from './SwipeCards';
+
+// const ShelterList = () => {
+
+// }
